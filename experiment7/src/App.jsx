66562e091fedf2f499d1895d6a466f1210770aef
Aniket2608 <aniketguleria392002@gmail.com>
2025-09-20 21:32:14 +0530

    Update App.jsx

--- experiment7/src/App.jsx
+++ experiment7/src/App.jsx
@@ -1,1 +1,21 @@
+import ProductCard from "./ProductCard";
+import "./App.css";
 
+function App() {
+  const products = [
+    { name: "Laptop", price: 60000, instock: true },
+    { name: "Smartphone", price: 25000, instock: false },
+    { name: "Headphone", price: 3000, instock: true }
+  ];
+
+  return (
+    <div className="app-container">
+      <h2 className="title">Products List</h2>
+      <div className="product-list">
+        <ProductCard products={products} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
