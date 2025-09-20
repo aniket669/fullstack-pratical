9944ef6a688eb4dfb8456c0612dd31799710b903
Aniket2608 <aniketguleria392002@gmail.com>
2025-09-20 21:35:43 +0530

    Update product card.jsx

--- experiment7/src/product card.jsx
+++ experiment7/src/product card.jsx
@@ -1,1 +1,15 @@
-
+const ProductCard = ({ products }) => {
+    return (
+      <div className="cards-container">
+        {products.map((product, index) => (
+          <div className="card" key={index}>
+            <h3 className="product-name">{product.name}</h3>
+            <p>Price: ₹{product.price}</p>
+            <p>Status: {product.instock ? "In Stock" : "Out of Stock"}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default ProductCard;
