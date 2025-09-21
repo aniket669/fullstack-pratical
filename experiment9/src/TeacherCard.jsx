a36546c64fb94f692acc25c11e2c221777dc7889
Aniket2608 <aniketguleria392002@gmail.com>
2025-09-21 22:27:14 +0530

    Update TeacherCard.jsx

--- experiment9/src/TeacherCard.jsx
+++ experiment9/src/TeacherCard.jsx
@@ -1,1 +1,12 @@
-
+function TeacherCard({ teacher }) {
+    return (
+      <div className="card">
+        <h3>Teacher</h3>
+        <p><b>Name:</b> {teacher.name}</p>
+        <p><b>Age:</b> {teacher.age}</p>
+        <p><b>Subject:</b> {teacher.subject}</p>
+        <p><b>Salary:</b> ₹{teacher.salary}</p>
+      </div>
+    );
+  }
+  export default TeacherCard;
