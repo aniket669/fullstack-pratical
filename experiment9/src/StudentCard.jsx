07772900f51ff713213767aa71b030bf5a3b4c46
Aniket2608 <aniketguleria392002@gmail.com>
2025-09-21 22:28:37 +0530

    Update StudentCard.jsx

--- experiment9/src/StudentCard.jsx
+++ experiment9/src/StudentCard.jsx
@@ -1,1 +1,12 @@
-
+function StudentCard({ student }) {
+    return (
+      <div className="card">
+        <h3>Student</h3>
+        <p><b>Name:</b> {student.name}</p>
+        <p><b>Age:</b> {student.age}</p>
+        <p><b>Roll No:</b> {student.rollNo}</p>
+        <p><b>Course:</b> {student.course}</p>
+      </div>
+    );
+  }
+  export default StudentCard;
